b124f427714b61bcd3ea54c1894db000bd8933ed
sukhjeetsekhon <sjtsekhon@gmail.com>
2026-07-12 20:49:06 -0700

    fix: reconfigure CMakeLists.txt file and rebuild project

--- cmake/stm32cubemx/cmake_install.cmake
+++ cmake/stm32cubemx/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/TritonBots-2027")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/STM32H723ZG_nucleo")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
