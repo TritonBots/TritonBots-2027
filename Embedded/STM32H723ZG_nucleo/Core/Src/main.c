ac20b159fffd2095cb45f16d34362d4f325df16f
sukhjeetsekhon <sjtsekhon@gmail.com>
2026-07-14 16:06:02 -0700

    feat: config SPI RX DMA

--- Embedded/STM32H723ZG_nucleo/Core/Src/main.c
+++ Embedded/STM32H723ZG_nucleo/Core/Src/main.c
@@ -55,6 +55,7 @@ FDCAN_HandleTypeDef hfdcan1;
 
 SPI_HandleTypeDef hspi1;
 DMA_HandleTypeDef hdma_spi1_tx;
+DMA_HandleTypeDef hdma_spi1_rx;
 
 TIM_HandleTypeDef htim2;
 
@@ -490,6 +491,9 @@ static void MX_DMA_Init(void)
   /* DMA1_Stream0_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(DMA1_Stream0_IRQn, 5, 0);
   HAL_NVIC_EnableIRQ(DMA1_Stream0_IRQn);
+  /* DMA1_Stream1_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Stream1_IRQn, 5, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Stream1_IRQn);
 
 }
 
