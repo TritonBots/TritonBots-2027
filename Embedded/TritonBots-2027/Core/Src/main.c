5daab58e3a53e55ca7f6c7b17f065042c6f3b8af
sukhjeetsekhon <sjtsekhon@gmail.com>
2026-07-21 20:04:11 -0700

    feat: add C3-C5 pitch frequency for pwm buzzer

--- Embedded/TritonBots-2027/Core/Src/main.c
+++ Embedded/TritonBots-2027/Core/Src/main.c
@@ -132,6 +132,25 @@ int main(void)
 
       $$Duty(\%)=\frac{100}{99+1}\times100=100\%$$
     
+
+      I want to use pwm signals to play musical pitches with a passive buzzer.
+
+      Note: Frequency (Hz)
+      C3: 130.81
+      D3: 146.83
+      E3: 164.81
+      F3: 174.61
+      G3: 196.00
+      A3: 220.00
+      B3: 246.94
+      C4: 261.63
+      D4: 293.67
+      E4: 329.63
+      F4: 349.23
+      G4: 392.00
+      A4: 440.00
+      B4: 493.88
+      C5: 523.25
     */
     
     for (int duty = 0; duty <= max_ccr*2; duty += 1)
