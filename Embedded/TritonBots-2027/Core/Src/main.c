bc3d2ee7e6afc442ca8c36f819cfc3cfa2b91c10
sukhjeetsekhon <sjtsekhon@gmail.com>
2026-07-26 14:16:21 -0700

    todo: code isn't working. The OLED doesn't turn on for some reason.

--- Embedded/TritonBots-2027/Core/Src/main.c
+++ Embedded/TritonBots-2027/Core/Src/main.c
@@ -24,6 +24,7 @@
 #include "ssd1306.h"
 #include "ssd1306_tests.h"
 #include "ssd1306_fonts.h"
+#include "stm32f1xx_hal_gpio.h"
 #include <stdbool.h>
 #include <stdio.h>
 /* USER CODE END Includes */
@@ -35,7 +36,8 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
+#define DHT11_PORT GPIOA
+#define DHT11_PIN GPIO_PIN_1
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -51,6 +53,8 @@
 /* Private variables ---------------------------------------------------------*/
 I2C_HandleTypeDef hi2c1;
 
+TIM_HandleTypeDef htim1;
+
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
@@ -59,8 +63,14 @@ I2C_HandleTypeDef hi2c1;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_I2C1_Init(void);
+static void MX_TIM1_Init(void);
 /* USER CODE BEGIN PFP */
-
+void delay_us(uint16_t us);
+void DHT11_Start(void);
+uint8_t DHT11_Check_Response(void);
+uint8_t DHT11_Read (void);
+void Set_Pin_Input(GPIO_TypeDef * PORTx, uint16_t GPIOx);
+void Set_Pin_Output(GPIO_TypeDef * PORTx, uint16_t GPIOx);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -98,26 +108,67 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_I2C1_Init();
+  MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
-  int count = 0;
-  char str[12];
-  HAL_GPIO_WritePin(GPIOC,GPIO_PIN_13,SET);
+
+  char Humidity_str[12];
+  char Temperature_str[12];
+
+  uint8_t Presence = 0;
+  uint8_t RH = 0;
+  uint8_t Rh_byte1 = 0;
+  uint8_t Rh_byte2 = 0;
+  uint8_t TEMP = 0;
+  uint8_t Temp_byte1 = 0;
+  uint8_t Temp_byte2 = 0;
+  uint8_t SUM = 0;
+  float Temperature = 2.2;
+  float Humidity = 1.1;
+  HAL_TIM_Base_Start(&htim1);  // for us Delay
+  ssd1306_Init();
+  HAL_Delay(2000);
+  // GPIO for DHT11 is pin A1
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, RESET);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    snprintf(str, sizeof(str), "%d", count++);
-    ssd1306_Fill(Black);
-    ssd1306_SetCursor(0, 0);
-    ssd1306_WriteString(str, Font_16x26, White);
-    ssd1306_UpdateScreen();
-
-    HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
-    HAL_Delay(100);
     HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
     HAL_Delay(1000);
+    HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+
+    // DHT11_Start();
+    // Presence = DHT11_Check_Response();
+    // Rh_byte1 = DHT11_Read();
+    // Rh_byte2 = DHT11_Read();
+    // Temp_byte1 = DHT11_Read();
+    // Temp_byte2 = DHT11_Read();
+    // SUM = DHT11_Read();
+
+    // TEMP = Temp_byte1;
+    // RH = Rh_byte1;
+
+    // Temperature = (float) TEMP;
+    // Humidity = (float) RH;
+
+    snprintf(Humidity_str, 12, "%1.2f", Humidity);
+    snprintf(Temperature_str, 12, "%1.2f", Temperature);
+
+    ssd1306_Fill(Black);
+    ssd1306_SetCursor(0, 0);
+    ssd1306_WriteString(Temperature_str, Font_16x26, White);
+    ssd1306_SetCursor(0, 16);
+    ssd1306_WriteString(Humidity_str, Font_16x26, White);
+    ssd1306_UpdateScreen();
+
+
+
+    HAL_Delay(1000);
+    
+
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -140,7 +191,9 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL12;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -150,12 +203,12 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
   {
     Error_Handler();
   }
@@ -196,6 +249,52 @@ static void MX_I2C1_Init(void)
 }
 
 /**
+  * @brief TIM1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM1_Init(void)
+{
+
+  /* USER CODE BEGIN TIM1_Init 0 */
+
+  /* USER CODE END TIM1_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM1_Init 1 */
+
+  /* USER CODE END TIM1_Init 1 */
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 48-1;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 65535;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM1_Init 2 */
+
+  /* USER CODE END TIM1_Init 2 */
+
+}
+
+/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
@@ -209,11 +308,14 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : PC13 */
   GPIO_InitStruct.Pin = GPIO_PIN_13;
@@ -221,6 +323,13 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PA1 */
+  GPIO_InitStruct.Pin = GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PB12 */
   GPIO_InitStruct.Pin = GPIO_PIN_12;
@@ -234,7 +343,81 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+void Set_Pin_Input(GPIO_TypeDef * PORTx, uint16_t GPIOx) {
+  if (GPIOx < 8) {
+    PORTx->CRL &= ~(0xF << (GPIOx * 4));        // Clear CNF + MODE bits
+    PORTx->CRL |=  (0x4 << (GPIOx * 4));        // CNF=01 (floating input), MODE=00
+  } else {
+    uint16_t pin = GPIOx - 8;
+    PORTx->CRH &= ~(0xF << (pin * 4));
+    PORTx->CRH |=  (0x4 << (pin * 4));
+  }
+}
 
+void Set_Pin_Output(GPIO_TypeDef * PORTx, uint16_t GPIOx) {
+  if (GPIOx < 8) {
+    PORTx->CRL &= ~(0xF << (GPIOx * 4));        // Clear CNF + MODE bits
+    PORTx->CRL |=  (0x3 << (GPIOx * 4));        // CNF=00 (push-pull), MODE=11 (50MHz)
+  } else {
+    uint16_t pin = GPIOx - 8;
+    PORTx->CRH &= ~(0xF << (pin * 4));
+    PORTx->CRH |=  (0x3 << (pin * 4));
+  }
+}
+
+void delay_us(uint16_t us)
+{
+	__HAL_TIM_SET_COUNTER(&htim1,0);  // set the counter value a 0
+	while (__HAL_TIM_GET_COUNTER(&htim1) < us);  // wait for the counter to reach the us input in the parameter
+}
+
+void DHT11_Start(void)
+{
+	Set_Pin_Output(DHT11_PORT, DHT11_PIN);       // set the pin as output
+	HAL_GPIO_WritePin(DHT11_PORT, DHT11_PIN, 0); // pull the pin low     
+	delay_us(18000);                                // wait for 18ms        
+	Set_Pin_Input(DHT11_PORT, DHT11_PIN);        // set as input
+}
+
+uint8_t DHT11_Check_Response(void)
+{
+	uint8_t Response = 0;
+	delay_us(40);
+	if (!(HAL_GPIO_ReadPin(DHT11_PORT, DHT11_PIN))) {
+		delay_us(80);
+		if ((HAL_GPIO_ReadPin(DHT11_PORT, DHT11_PIN))) {
+      Response = 1;
+    } else {
+      Response = -1;
+    }
+	}
+	while((HAL_GPIO_ReadPin (DHT11_PORT, DHT11_PIN))) {
+    // wait for the pin to go low
+  } 
+
+	return Response;
+}
+
+uint8_t DHT11_Read(void)
+{
+	uint8_t i;
+  uint8_t j;
+	for (j=0;j<8;j++) {
+		while (!(HAL_GPIO_ReadPin (DHT11_PORT, DHT11_PIN))) {
+      // wait for the pin to go high
+    }
+		delay_us(40);   // wait for 40 us
+		if (!(HAL_GPIO_ReadPin (DHT11_PORT, DHT11_PIN))) {
+			i&= ~(1<<(7-j));   // if the pin is low write 0
+		} else {
+      i|= (1<<(7-j)); // if the pin is high, write 1
+    }
+		while ((HAL_GPIO_ReadPin (DHT11_PORT, DHT11_PIN))) {
+      // wait for the pin to go low
+    }
+	}
+	return i;
+}
 /* USER CODE END 4 */
 
 /**
