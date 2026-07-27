bbf4d9811f756f0fb4e4fc836f82826abd42e34c
sukhjeetsekhon <sjtsekhon@gmail.com>
2026-07-26 22:49:11 -0700

    bug: code is not reading correctly, but it is able to run through all of the functions without getting stuck

--- Embedded/TritonBots-2027/Core/Src/main.c
+++ Embedded/TritonBots-2027/Core/Src/main.c
@@ -37,6 +37,8 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define DHT11_PORT GPIOA
+#define DHT11_PIN GPIO_PIN_1
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -68,7 +70,76 @@ static void MX_TIM1_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+void delay_us(uint16_t time) {
+  __HAL_TIM_SET_COUNTER(&htim1, 0);
+  while ((__HAL_TIM_GET_COUNTER(&htim1)) < time);
+}
 
+uint8_t Rh_byte1, Rh_byte2, Temp_byte1, Temp_byte2;
+uint16_t SUM, RH, TEMP;
+char temp[10];
+char rh[10];
+
+float Temperature = 0;
+float Humidity = 0;
+uint8_t Presence = 0;
+
+void Set_Pin_Output(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin) {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  GPIO_InitStruct.Pin = GPIO_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOx, &GPIO_InitStruct);
+}
+
+void Set_Pin_Input(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin) {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  GPIO_InitStruct.Pin = GPIO_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Speed = GPIO_NOPULL;// TODO: this might be NOPULL
+  HAL_GPIO_Init(GPIOx, &GPIO_InitStruct);
+}
+
+void DHT11_Start(void) {
+  Set_Pin_Output(DHT11_PORT, DHT11_PIN);
+  HAL_GPIO_WritePin(DHT11_PORT, DHT11_PIN, RESET);
+  delay_us(18000); // 18 ms
+  HAL_GPIO_WritePin(DHT11_PORT, DHT11_PIN, SET);
+  delay_us(20); 
+  Set_Pin_Input(DHT11_PORT, DHT11_PIN);
+}
+
+uint8_t DHT11_Check_Response(void) {
+  uint8_t Response = 0;
+  delay_us(40);
+  if ((HAL_GPIO_ReadPin(DHT11_PORT, DHT11_PIN))) {
+    delay_us(80);
+    if ((HAL_GPIO_ReadPin(DHT11_PORT, DHT11_PIN))) {
+      Response = 1;
+    } else {
+      Response = -1; // 255
+    }
+  }
+
+  while(!(HAL_GPIO_ReadPin(DHT11_PORT, DHT11_PIN)));
+
+  return Response;
+}
+
+uint8_t DHT11_Read(void) {
+  uint8_t i,j;
+  for (j=0;j<8;j++) {
+    while(!(HAL_GPIO_ReadPin(DHT11_PORT, DHT11_PIN)));
+    delay_us(40);
+    if (!(HAL_GPIO_ReadPin(DHT11_PORT, DHT11_PIN)))
+    {
+      i &= ~(1<<(7-j));
+    }
+    else i |= (1<<(7-j));
+    while (!(HAL_GPIO_ReadPin(DHT11_PORT, DHT11_PIN)));
+  }
+  return i;
+}
 /* USER CODE END 0 */
 
 /**
@@ -103,13 +174,50 @@ int main(void)
   MX_I2C1_Init();
   MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
+  HAL_TIM_Base_Start(&htim1);
+  ssd1306_Init();
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, SET);
 
+  ssd1306_Fill(Black);
+  ssd1306_SetCursor(0, 0);
+  ssd1306_WriteString("START", Font_16x26, White);
+  ssd1306_UpdateScreen();
+
+  HAL_Delay(2000); // TODO: delete later
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+    DHT11_Start();
+    Presence = DHT11_Check_Response();
+
+    Rh_byte1 = DHT11_Read();
+    Rh_byte2 = DHT11_Read();
+    Temp_byte1 = DHT11_Read();
+    Temp_byte2 = DHT11_Read();
+    SUM = DHT11_Read();
+
+    TEMP = Temp_byte1;
+    RH = Rh_byte1;
+
+    Temperature = (float) TEMP;
+    Humidity = (float) RH;
+
+    snprintf(temp, 10, "%.3f", Temperature);
+    snprintf(rh, 10, "%.3f", Humidity);
+
+    ssd1306_Fill(Black);
+    ssd1306_SetCursor(0, 0);
+    ssd1306_WriteString(temp, Font_16x26, White);
+    ssd1306_SetCursor(0, 32);
+    ssd1306_WriteString(rh, Font_16x26, White);
+    ssd1306_UpdateScreen();
+
+    HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+    HAL_Delay(1000);
+    HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
     
     /* USER CODE END WHILE */
 
